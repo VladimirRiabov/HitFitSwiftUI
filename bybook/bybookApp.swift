26a1bffe3f618b2668a63fc3d98d9b749050d56c
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-22 15:01:35 +0300

    Initial Commit

---
+++ bybook/bybookApp.swift
@@ -0,0 +1,17 @@
+//
+//  bybookApp.swift
+//  bybook
+//
+//  Created by Владимир Рябов on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct bybookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
