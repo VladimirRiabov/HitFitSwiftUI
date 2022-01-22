7af4d8586f7176ad0d16fc7e02b030da005cad87
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-22 17:09:42 +0300

    extract the header code to create a HeaderView

--- bybook/Views/ExerciseView.swift
+++ bybook/Views/ExerciseView.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 struct ExerciseView: View {
     let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
-   let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+    let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
     let index: Int
     var body: some View {
-        Text(exerciseNames[index])
+        VStack {
+            VStack {
+                Text(exerciseNames[index])
+                    .font(.largeTitle)
+            }
+            HStack {
+                Image(systemName: "1.circle")
+                    .font(.largeTitle)
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+                Image(systemName: "4.circle")
+            }
+            .font(.title2)
+            Text("Video player")
+            Text("Timer")
+            Text("Start/Done button")
+            Text("Rating")
+            Text("History button")
+        }
     }
 }
 
